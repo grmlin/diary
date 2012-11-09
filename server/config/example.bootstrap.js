f6fb26a3ec36fbdb6aa83e72aa7f41c384ec2974
hrdwr <mail@andreas-wehr.de>
2012-11-09 10:33:52 +0100

    docs

--- server/config/example.bootstrap.js
+++ server/config/example.bootstrap.js
@@ -1,8 +1,15 @@
 /*
+
+Add users/editors by adding the `DEFAULT_USER` variable to the global scope as you can see below. 
+
 DEFAULT_USER = [
     {
         "username" : "foo",
         "email" : "bar@mail.com"
+    },
+    {
+        "username": "me",
+        "email": "me@you.com"
     }
 ]
     
